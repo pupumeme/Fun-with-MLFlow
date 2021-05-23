49d0a0daa58051433815d1ff8cd3a5da06f44252
pupumeme <45849306+pupumeme@users.noreply.github.com>
2021-05-23 12:14:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/pupumeme/Fun-with-MLFlow.git
 cd Fun-with-MLFlow/
+sudo apt-get update -y 
+sudo apt-get install docker.io -y 
 sudo docker build -t mlflow-docker . --no-cache
-sudo docker run -p 5050:8888 mlflow-docker
